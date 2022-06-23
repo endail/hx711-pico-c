6e20b2e224225801dc56307b0ca74fddab8f30da
endail <52652357+endail@users.noreply.github.com>
2022-06-23 16:46:47 +1000

    asserts; remove loop unroll

--- src/util.c
+++ src/util.c
@@ -30,19 +30,10 @@ void util_average(
 
         assert(arr != NULL);
         assert(len > 0);
+        assert(avg != NULL);
 
         long long int total = 0;
 
-        /**
-         * Why set unroll to 2?
-         * 
-         * util_median may call util_average when calculating
-         * the median of the middle two elements. So there is
-         * a good chance util_average(arr, 2, avg) will be
-         * called much more often than with a len > 2. In
-         * these cases it is much more efficient to unroll.
-         */
-        #pragma GCC unroll (2)
         for(size_t i = 0; i < len; ++i) {
             total += arr[i];
         }
@@ -58,6 +49,7 @@ void util_median(
 
         assert(arr != NULL);
         assert(len > 0);
+        assert(med != NULL);
 
         qsort(
             arr,
