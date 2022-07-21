d1838150cf131abb3cead01293388bde00843b13
endail <52652357+endail@users.noreply.github.com>
2022-07-21 16:40:37 +1000

    fix test

--- generate_pio.js
+++ generate_pio.js
@@ -1,10 +1,12 @@
 
-import { PIOAssembler } from "pioasm";
+var PIOAssembler = require("pioasm");
 var fs = require("fs");
 
+console.log(PIOAssembler);
+
 const source = fs.readFileSync(
-    "src/hx711_noblock.pio",
-    "utf8");
+"src/hx711_noblock.pio",
+"utf8");
 
 const pioasm = new PIOAssembler();
     pioasm.assemble(source).then(result => {
