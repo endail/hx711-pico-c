465b7d4dd8cc058d0b47df88d482c6bd5aea8274
endail <52652357+endail@users.noreply.github.com>
2022-08-11 05:48:56 +0000

    Deploying to gh-pages from  @ c770b2d9559159852f6e6e72c3164c8074db8a9d 🚀

--- hx711_8c.js
+++ hx711_8c.js
@@ -2,7 +2,7 @@ var hx711_8c =
 [
     [ "hx711_close", "hx711_8c.html#a430408c58853bd2ec9b4e2bacb36f055", null ],
     [ "hx711_get_value", "hx711_8c.html#a2a490345d56d3d9f06f809c07446a6e5", null ],
-    [ "hx711_get_value_timeout", "hx711_8c.html#a883615f6bc134c760b61e5eefa0443f4", null ],
+    [ "hx711_get_value_timeout", "hx711_8c.html#af65c2fd8ea8914dd0d4fbe92e1ab1493", null ],
     [ "hx711_init", "hx711_8c.html#a20c562c58345b4cb4c8922925ec0a877", null ],
     [ "hx711_set_gain", "hx711_8c.html#a02e3733a8c520e26cb44caac32945c63", null ],
     [ "hx711_set_power", "hx711_8c.html#aa47929fdc96d757b9306dd57ef752a20", null ]
