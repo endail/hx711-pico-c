2a6f1be0b418ec46f4d17d2dcbede23b6b953b9b
endail <52652357+endail@users.noreply.github.com>
2024-12-21 19:52:55 +1100

    optimise conversion

--- include/hx711.h
+++ include/hx711.h
@@ -150,7 +150,7 @@ void hx711_set_gain(
  */
 inline int32_t hx711_convert_raw(
     const uint32_t raw) {
-        return (int32_t)(raw << 8) >> 8;
+        return (int32_t)(((int32_t)raw << 8) >> 8);
 }
 
 /**
